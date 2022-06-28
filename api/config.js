eb4c2583798068f00fb696eb2db2428999605627
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-06-29 01:40:49 +0600

    Added basic stucture of back

---
+++ api/config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const rootPath = __dirname;
+
+let dbUrl = 'mongodb://localhost/exam-13';
+let port = 8000;
+
+if (process.env.NODE_ENV === 'test') {
+    dbUrl = 'mongodb://localhost/exam-13-test';
+    port = 8010;
+}
+
+module.exports = {
+    port,
+    corsWhitelist: [
+        'http://localhost:4200',
+        'https://localhost:4200',
+        'http://localhost:4210',
+    ],
+    rootPath,
+    uploadPath: path.join(rootPath, 'public/uploads'),
+    mongo: {
+        db: dbUrl,
+        options: {useNewUrlParser: true},
+    },
+    facebook: {
+        appId: '718422199316941',
+        appSecret: 'e822dc5e22ab51742e7d8b8ec4d8b6f9'
+    }
+};
---
+++ api/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const config = require('./config');
+const app = express();
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (origin === undefined || config.corsWhitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+const run = async () => {
+    await mongoose.connect(config.mongo.db, config.mongo.options);
+
+    app.listen(config.port, () => {
+        console.log(`Server started on ${config.port} port!`);
+    });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    });
+};
+
+run().catch(e => console.error(e));
