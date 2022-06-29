c5890882b674b2bf7d23d8b2eb959faa2c3afdfd
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-06-29 14:18:48 +0600

    Added auth-interceptor

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -9,22 +9,54 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexModule } from '@angular/flex-layout';
 import { HasRolesDirective } from './directives/has-roles.directive';
 import { UserTypeDirective } from './directives/user-type.directive';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { AppStoreModule } from './app-store.module';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { AuthInterceptor } from './auth.interceptor';
+import { FacebookLoginProvider, SocialAuthServiceConfig } from 'angularx-social-login';
+import { environment } from '../environments/environment';
+
+const socialConfig: SocialAuthServiceConfig = {
+  autoLogin: false,
+  providers: [
+    {
+      id: FacebookLoginProvider.PROVIDER_ID,
+      provider: new FacebookLoginProvider(environment.fbAppId, {
+        scope: 'email,public_profile'
+      })
+    }
+  ]
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
     HasRolesDirective,
-    UserTypeDirective
+    UserTypeDirective,
   ],
   imports: [
-    BrowserModule,
+    AppStoreModule,
     AppRoutingModule,
+    HttpClientModule,
+    BrowserModule,
     MatSidenavModule,
     MatToolbarModule,
-    FlexModule
+    MatSnackBarModule,
+    FlexModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatListModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: 'SocialAuthServiceConfig', useValue: socialConfig },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
