b23b610c0cdb536fde47e4d543410ced3cb18d1b
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-07-02 22:24:37 +0600

    Added fixtures data

--- api/fixtures.js
+++ api/fixtures.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const config = require("./config");
 const User = require("./models/User");
 const Place = require("./models/Place");
+const Review = require("./models/Review");
 
 const run = async () => {
     await mongoose.connect(config.mongo.db, config.mongo.options);
@@ -28,7 +29,8 @@ const run = async () => {
         role: 'admin'
     });
 
-    await Place.create({
+
+    const [AMELA, COFFEE_SHOP, KANEL, MY_COFFEE_1, AMELA_2, COFFEE_SHOP_2, KANEL_2, MY_COFFEE_2] = await Place.create({
         owner: GROGU,
         title: 'Amela',
         description: 'We offer more than a great cup of coffee, we offer ' +
@@ -53,7 +55,55 @@ const run = async () => {
         images: [],
         reviews: [],
     }, {
+        owner: ADMIN,
+        title: 'Kanel',
+        description: 'We offer more than a great cup of coffee, we offer ' +
+            'a warm and welcoming spot to meet old friends and make new ones.' +
+            ' That\'s why folks often say "I\'ll meet you at the coffee shop" when planning' +
+            ' a get together with friends, colleagues or business associates.' +
+            ' It\'s been that way since Amelia Island Coffee opened 18 years ago.',
+        mainPhoto: 'kanel.jpg',
+        ratings: [],
+        images: [],
+        reviews: [],
+    }, {
         owner: GROGU,
+        title: 'My Coffee',
+        description: 'We offer more than a great cup of coffee, we offer ' +
+            'a warm and welcoming spot to meet old friends and make new ones.' +
+            ' That\'s why folks often say "I\'ll meet you at the coffee shop" when planning' +
+            ' a get together with friends, colleagues or business associates.' +
+            ' It\'s been that way since Amelia Island Coffee opened 18 years ago.',
+        mainPhoto: 'mycoffee.jpg',
+        ratings: [],
+        images: [],
+        reviews: [],
+    }, {
+        owner: ADMIN,
+        title: 'Kanel',
+        description: 'We offer more than a great cup of coffee, we offer ' +
+            'a warm and welcoming spot to meet old friends and make new ones.' +
+            ' That\'s why folks often say "I\'ll meet you at the coffee shop" when planning' +
+            ' a get together with friends, colleagues or business associates.' +
+            ' It\'s been that way since Amelia Island Coffee opened 18 years ago.',
+        mainPhoto: 'kanel.jpg',
+        ratings: [],
+        images: [],
+        reviews: [],
+    }, {
+        owner: GROGU,
+        title: 'My Coffee',
+        description: 'We offer more than a great cup of coffee, we offer ' +
+            'a warm and welcoming spot to meet old friends and make new ones.' +
+            ' That\'s why folks often say "I\'ll meet you at the coffee shop" when planning' +
+            ' a get together with friends, colleagues or business associates.' +
+            ' It\'s been that way since Amelia Island Coffee opened 18 years ago.',
+        mainPhoto: 'mycoffee.jpg',
+        ratings: [],
+        images: [],
+        reviews: [],
+    }, {
+        owner: ADMIN,
         title: 'Kanel',
         description: 'We offer more than a great cup of coffee, we offer ' +
             'a warm and welcoming spot to meet old friends and make new ones.' +
@@ -78,6 +128,111 @@ const run = async () => {
         reviews: [],
     });
 
+
+    const [review1, review2, review3,
+        review4, review5, review6,
+        review7, review8, review9,
+        review10, review11, review12,
+    ] = await Review.create({
+        author: GROGU,
+        place: AMELA,
+        description: 'Really good place',
+        ratings: {
+            overAll: 5,
+            food: 5,
+            service: 5,
+            interior: 5,
+        }
+    }, {
+        author: GROGU,
+        place: AMELA,
+        description: 'Really good place',
+        ratings: {
+            overAll: 5,
+            food: 5,
+            service: 5,
+            interior: 5,
+        }
+    }, {
+        author: GROGU,
+        place: AMELA,
+        description: 'Really good place',
+        ratings: {
+            overAll: 5,
+            food: 5,
+            service: 5,
+            interior: 5,
+        }
+    }, {
+        author: GROGU,
+        place: KANEL,
+        description: 'Really good place',
+        ratings: {
+            overAll: 5,
+            food: 5,
+            service: 5,
+            interior: 5,
+        }
+    }, {
+        author: GROGU,
+        place: KANEL,
+        description: 'Really good place',
+        ratings: {
+            overAll: 5,
+            food: 5,
+            service: 5,
+            interior: 5,
+        }
+    }, {
+        author: GROGU,
+        place: COFFEE_SHOP,
+        description: 'Really good place',
+        ratings: {
+            overAll: 5,
+            food: 5,
+            service: 5,
+            interior: 5,
+        }
+    }, {
+        author: GROGU,
+        place: COFFEE_SHOP,
+        description: 'Really good place',
+        ratings: {
+            overAll: 5,
+            food: 5,
+            service: 5,
+            interior: 5,
+        }
+    }, {
+        author: GROGU,
+        place: COFFEE_SHOP,
+        description: 'Really good place',
+        ratings: {
+            overAll: 5,
+            food: 5,
+            service: 5,
+            interior: 5,
+        }
+    },)
+
+    AMELA.reviews = [
+        review1,
+        review2,
+        review4,
+        review5,
+        review6,
+    ];
+  const reviews = [
+        review1,
+        review2,
+        review4,
+        review5,
+        review6,
+    ];
+
+    AMELA.set('reviews', reviews);
+
+    AMELA.update({reviews});
 
     await mongoose.connection.close();
 };
